44b5e3511e61222ca581e6cf625174e1f54fa0ae
zx <zx@163.com>
2018-11-01 20:33:20 +0800

    2018-11-1 20:33:13 备份

--- tb-tun/uml_bbr_one_click_script.sh
+++ tb-tun/uml_bbr_one_click_script.sh
@@ -2,14 +2,11 @@
 #======================================================================#
 # 注意: 脚本需要以 UTF-8 编码, 并以 LF 结尾.
 #======================================================================#
-APPDIR="/root/tbtest"
-IFNAME="tbtest"
+AppDir="/root/tb_tun"
+IFNAME="tb-tun"
 Server_IPv4_Address="74.82.46.6"
 Client_IPv4_Address="1.119.141.146"
 Client_IPv6_Address="2001:470:23:11ca::2/64"
-
-
-
 
 
 
@@ -17,6 +14,12 @@ function fun_check(){
     wget --help  > /dev/null 2>&1
     if [ $? -ne 0 ]; then
         echo "[ERROR]:${LINENO}, Please install wget software." 1>&2
+        exit 1
+    fi
+
+    gcc --version > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "[ERROR]:${LINENO}, Please install gcc software." 1>&2
         exit 1
     fi
 
@@ -33,6 +36,13 @@ function fun_stop() {
     ip link set ${IFNAME} down
     if [ $? -ne 0 ]; then echo "[ERROR]:${LINENO}"; return 1; fi
 
+    # 猜测: 找到默认路由的名字 (D_I => DefaultInterface ?) (0/0 => 0.0.0.0/0 ?)
+    D_I=$(ip route show exact 0/0 | sort -k 7 | head -n 1 | sed -n 's/^default.* dev \([^ ]*\).*/\1/p')
+    if [ "${D_I}" == "" ]; then echo "[ERROR]:${LINENO}"; return 1; fi
+
+    ip -6 route add ::/0 dev ${D_I}
+    if [ $? -ne 0 ]; then echo "[ERROR]:${LINENO}"; return 1; fi
+
     ip -6 route del ::/0 dev ${IFNAME}
     if [ $? -ne 0 ]; then echo "[ERROR]:${LINENO}"; return 1; fi
 
@@ -46,7 +56,7 @@ function fun_stop() {
 function fun_start() {
     echo "action [start] BEG..."
 
-    setsid ${APP_DIR}/tb_userspace ${IFNAME} ${Server_IPv4_Address} ${Client_IPv4_Address} sit > /dev/null 2>&1 &
+    setsid ${AppDir}/tb_userspace ${IFNAME} ${Server_IPv4_Address} ${Client_IPv4_Address} sit > /dev/null 2>&1 &
     if [ $? -ne 0 ]; then echo "[ERROR]:${LINENO}"; return 1; fi
 
     sleep 1s
@@ -65,7 +75,6 @@ function fun_start() {
     # 设置::/0网段的数据走${IFNAME}
     ip -6 route add ::/0 dev ${IFNAME}
 
-    # 猜测: 找到默认路由的名字 (D_I => DefaultInterface ?) (0/0 => 0.0.0.0/0 ?)
     D_I=$(ip route show exact 0/0 | sort -k 7 | head -n 1 | sed -n 's/^default.* dev \([^ ]*\).*/\1/p')
     if [ "${D_I}" == "" ]; then echo "[ERROR]:${LINENO}"; return 1; fi
 
@@ -80,13 +89,13 @@ function fun_start() {
 function fun_install() {
     echo "action [install] BEG..."
     
-    mkdir -p ${APP_DIR}
+    mkdir -p ${AppDir}
     if [ $? -ne 0 ]; then echo "[ERROR]:${LINENO}"; return 1; fi
     
-    wget --directory-prefix=${APP_DIR} https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/tb-tun/tb-tun_r18.tar.gz  > /dev/null 2>&1
+    wget --directory-prefix=${AppDir} https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/tb-tun/tb-tun_r18.tar.gz  > /dev/null 2>&1
     if [ $? -ne 0 ]; then echo "[ERROR]:${LINENO}"; return 1; fi
     
-    cd ${APP_DIR}
+    cd ${AppDir}
     if [ $? -ne 0 ]; then echo "[ERROR]:${LINENO}"; return 1; fi
     
     tar -xf tb-tun_r18.tar.gz
@@ -117,7 +126,7 @@ function func_run() {
         fun_stop  &&  fun_start
         ;;
     'install')
-        fun_stop  &&  fun_uninstall  &&  fun_install
+        fun_stop  &&  fun_install
         ;;
     *)
         echo "Usage: ${program} { start | stop | restart | install }"
